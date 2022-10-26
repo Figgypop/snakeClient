869b73cff9517bb1ed289ba8c3c609c7dac7422b
David Figueroa <figueroadavid93@gmail.com>
2022-10-26 13:52:51 -0400

    created a seperate file with connect function exporting to play file.

--- play.js
+++ play.js
@@ -1,25 +1,6 @@
-const net = require("net");
+const {connect} = require("./client.js");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.on("connect", () => {
-    console.log("connection succesful")
-  })
-
-  conn.on("data", (data) => {
-    console.log("Server says", data)
-  })
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
+  
 console.log("Connecting ...");
 connect();
+
